8a23f0e6aebc3c794b69364cf4d47f103027a1a3
brownrout <ericbrownrout2016@u.northwestern.edu>
2016-03-07 22:47:42 -0600

    reorganize main, add scores and human interface

--- test.py
+++ test.py
@@ -2,12 +2,7 @@
 from bs4 import BeautifulSoup
 import urllib
 
-def get_artist_info(dct):
-
-    #query user for the artist, and make sure it is url friendly
-    artist = str(raw_input('Which artist would you like to search: '))
-    dct['artist_name'] = artist
-    artist.replace(' ', '%')
+def get_artist_info(dct, artist):
     
     # create the new url for query
     r = urllib.urlopen("http://musicbrainz.org/ws/2/artist?query=artist:\""+artist+"\"").read()
@@ -29,18 +24,23 @@ def get_artist_info(dct):
         for child in element.children:
             dct['tags'].append(child.text.replace(' ', "%"))
 
-    url = "http://musicbrainz.org/ws/2/artist?query="
-    for x in dct['tags']:
-        url += "+tag:"+x
+    # build the query which will grab similar artists
+    url = "http://musicbrainz.org/ws/2/artist?query=tag:"
+    stop = len(dct['tags'])-1
+    for index, x in enumerate(dct['tags']):
+        url += x
+        if index != stop:
+            url += "%20AND%20tag:"
 
     # grab the similar artists
     r = urllib.urlopen(url).read()
     soup = BeautifulSoup(r, "lxml")
     artist_names = list()
     for element in soup.find_all("artist"):
+        _score = element.get('ext:score')
         _name = element.find("name").text
-        if _name.lower() != dct["artist_name"].lower():
-            artist_names.append(_name)
+        if _name.lower() != dct["artist_name"].lower() and int(_score) > 60:
+            artist_names.append([_name,_score])
     dct['similar artists'] = artist_names
 
 
@@ -53,14 +53,20 @@ def main():
 
     while True:
         answers = {}
-        print '\n'
-        print "\nOptions:\n1. Get Artist Info"
-        user_input = input("Choose a function: ")
+        #query user for the artist, and make sure it is url friendly
+        artist = str(raw_input('enter an artist: '))
+        answers['artist_name'] = artist
+        print "\noptions:\n1. find similar artists\n"
+        user_input = input("choose a function: ")
         if (user_input == 1):
-            get_artist_info(answers)
-            print answers
+            get_artist_info(answers, artist.replace(' ', '%'))
+            print "\n"
+            print "we found " + str(len(answers['similar artists'])) + " artists related to " + answers['artist_name'].lower() + ":"
+            for x in answers['similar artists']:
+                print x[0].lower() + " - score: " + str(x[1])
+            print "\n"
         else:
-            print "Invalid choice\n"
+            print "invalid choice\n"
     
     return
 
